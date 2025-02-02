3b8254df49cb88b231ee18e3053e129af7a49262
Oluwaseun Otetumo <annyauthe@yahoo.com>
2025-02-03 00:49:55 +0100

    Fix square

--- square.py
+++ square.py
@@ -1,28 +1,34 @@
 #!/usr/bin/python3
+"""Module defines a Square class and methods."""
 
-class square():
-    
-    width = 0
-    height = 0
 
-    
+class Square():
+    """Defines a square class with dynamic attributes."""
+
     def __init__(self, *args, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+        """Initializes a square where all sides are equal."""
+        if "width" in kwargs:
+            self.width = kwargs["width"]
+            self.height = kwargs["width"]
+        else:
+            self.width = 0
+            self.height = 0
 
     def area_of_my_square(self):
         """ Area of the square """
         return self.width * self.width
 
-    def PermiterOfMySquare(self):
-        return (self.width * 2) + (self.height * 2)
+    def perimeter_of_my_square(self):
+        """Calculate the perimeter of the square."""
+        return (self.width * 2) + (self.width * 2)
 
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
 
+
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.perimeter_of_my_square())
